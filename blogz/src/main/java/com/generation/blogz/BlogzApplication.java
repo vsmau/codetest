21010489db4a81dcabbc2d006d5428ea8b854253
Vinicius <boringmkt@gmail.com>
2024-12-15 09:34:57 -0300

    :white_check_mark: test criacao de spring initializer

---
+++ blogz/src/main/java/com/generation/blogz/BlogzApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogzApplication.class, args);
+	}
+
+}
